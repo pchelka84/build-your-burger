62346b7c6d8589259ba845c6d28e22530f93383f
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-01-26 13:09:55 -0500

    Adding top margin to the main section

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
+
 import Aux from "../../hoc/Auxiliaru";
+import classes from "./Layout.css";
 
 const layout = props => (
   <Aux>
     <div>Toolbar, SideDrawe, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
