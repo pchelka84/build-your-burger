0cb8d18f895b19b76e3882d037e6b697a38ade2b
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-01-29 11:50:40 -0500

    Outputting Build Controls

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,13 +1,13 @@
-import { React } from "react";
+import React from "react";
 
 import classes from "./BuildControl.css";
 
-const buildControl = props => {
+const buildControl = props => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
     <button className={classes.Less}>Less</button>
-    <button classname={classes.More}>More</button>
-  </div>;
-};
+    <button className={classes.More}>More</button>
+  </div>
+);
 
 export default buildControl;
