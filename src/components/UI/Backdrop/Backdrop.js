3657a466267c731e3a2475cab9e6b174df3e6f54
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-01-31 13:19:30 -0500

    Creating, styling, adding Backdrop

---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import backdrop from "./Backdrop.css";
+
+const backdrop = props => (props.show ? <div classname={classes.Backdrop/> : null);
+
+export default backdrop;
