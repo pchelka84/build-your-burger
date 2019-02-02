deee1322a1694d83123324afb37f077a8c47830d
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-02-02 16:52:03 -0500

    Styling navigation items

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-import classes from "./NavigationItem.css";
+import classes from "./NavigationItems.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
 
 const navigationItems = () => (
-  <ul className={classes.navigationItems}>
+  <ul className={classes.NavigationItems}>
     <NavigationItem link="/" active>
       Burger Builder
     </NavigationItem>
