4ded1d95262fa5dffd5e527b1a78bf79257e04ac
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-02-07 15:08:52 -0500

    Turning Laout to the stateful functional component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Aux from "../../hoc/Auxiliaru";
 import classes from "./Layout.css";
 import Toolbar from "./../Navigation/Toolbar/Toolbar";
 import SideDrawer from "./../Navigation/SideDrawer/SideDrawer";
 
-const layout = props => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {};
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer />
+        <main className={classes.Content}>{props.children}</main>
+      </Aux>
+    );
+  }
+}
 
-export default layout;
+export default Layout;
