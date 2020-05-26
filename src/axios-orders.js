6af9fcbdb37510dc4bd5efc50554455199a02c6f
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-05-26 15:14:52 -0400

    Create the axios intance

---
+++ src/axios-orders.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const instance = axios.create({
+
+  // that's where we want to store out data 
+  baseURL: 'https://build-your-burger-be86c.firebaseio.com/';
+})
+
+export default instance;
