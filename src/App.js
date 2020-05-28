efa0931dcaa981c3ba5c397910180ae07f456bc8
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-05-28 19:25:54 -0400

    Remove old interceptors

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
+  // testing interceptors removal when we don't need BurgerBuilder
+
+  // state = {
+  //   show: true,
+  // };
+
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({ show: false });
+  //   }, 5000);
+  // }
+
   render() {
     return (
       <div>
         <Layout>
           <BurgerBuilder />
         </Layout>
+        {/* <Layout>{this.state.show ? <BurgerBuilder /> : null}</Layout> */}
       </div>
     );
   }
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,21 +9,27 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null,
     };
 
-    componentDidMount = () => {
+    componentWillMount = () => {
       // set interceptor to clear an error from below
-      axios.interceptors.request.use((req) => {
+      this.reqInterceptor = axios.interceptors.request.use((req) => {
         this.setState({ error: null });
         return req;
       });
 
       // axios listener to handle errors globally
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         (res) => res,
         (error) => {
           this.setState({ error: error });
         }
       );
     };
+
+    componentWillUnmount() {
+      // console.log("Will unmount", this.reqInterceptor, this.resInterceptor);
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
+    }
 
     errorConfirmedHandler = () => {
       this.setState({ error: null });
