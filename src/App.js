ea34ec2936531f65b6a629a31f6d8edb51bdad7d
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-06-09 14:25:44 -0400

    Set up Routes

--- src/App.js
+++ src/App.js
@@ -3,28 +3,16 @@ import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import { Route } from "react-router-dom";
 
 class App extends Component {
-  // testing interceptors removal when we don't need BurgerBuilder
-
-  // state = {
-  //   show: true,
-  // };
-
-  // componentDidMount() {
-  //   setTimeout(() => {
-  //     this.setState({ show: false });
-  //   }, 5000);
-  // }
-
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Route path="/checkout" component={Checkout} />
         </Layout>
-        {/* <Layout>{this.state.show ? <BurgerBuilder /> : null}</Layout> */}
       </div>
     );
   }
