80489beb9c194eccfcb3ff7f6c40e16335f15b57
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-06-13 14:44:37 -0400

    Add dropdown component for delivery options

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -101,6 +101,7 @@ class ContactData extends Component {
       <form>
         {formElementsArray.map((formElement) => (
           <Input
+            key={formElement.id}
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
