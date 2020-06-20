01e8502eeabb7a6c6a34a3ebabcbeb194f06845e
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-06-20 15:33:14 -0400

    Update 'purchasable' local state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,6 @@ import * as actionTypes from "../../store/actions";
 
 class BurgerBuilder extends Component {
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: null,
@@ -30,7 +29,6 @@ class BurgerBuilder extends Component {
     //     this.setState({ error: true });
     //   });
   }
-
   updatePurchaseState = (ingredients) => {
     // turning the object into the array
     const sum = Object.keys(ingredients)
@@ -42,7 +40,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, element) => {
         return sum + element;
       }, 0);
-    this.setState({ purchasable: sum > 0 });
+    return sum > 0;
   };
 
   purchaseHandler = () => {
@@ -102,7 +100,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
             price={this.props.price}
           />
