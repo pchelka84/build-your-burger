49d3a16bff471e65fbf781d5357bbf514661915f
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-06-20 15:52:21 -0400

    Adjust Checkout & ContactData (Redux)

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import Button from "../../../components/UI/Button/Button";
 import classes from "./ContactData.css";
@@ -105,7 +106,7 @@ class ContactData extends Component {
       ].value;
     }
     const order = {
-      ingredients: this.props.ingredients,
+      ingredients: this.props.ings,
       // for production ( price: this.state.totalPrice;): calculate it on the server because there is
       // probably there are products stored on the server there
       // to make sure that the user isn't manipulating the code.
@@ -215,4 +216,10 @@ class ContactData extends Component {
   }
 }
 
-export default ContactData;
+const mapStateToProps = (state) => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice,
+  };
+};
+export default connect(mapStateToProps)(ContactData);
