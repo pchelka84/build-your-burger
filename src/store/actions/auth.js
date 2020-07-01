e2ada4bcb6aa4706edb6d6cc9c8bfd9073f2a19c
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-07-01 16:27:04 -0400

    Get a token from Firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import * as actiontTypes from "./actionTypes";
 
 export const authStart = () => {
@@ -23,5 +25,23 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
   return (dispatch) => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB_JoQedROh5AmwQh0Z66I8MAmbE-b_TBs",
+        authData
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
   };
 };
