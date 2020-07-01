91e0bd69e62e954f224d3691a51445702ea85fc6
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-07-01 18:08:51 -0400

    Add sign in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return (dispatch) => {
     dispatch(authStart());
     const authData = {
@@ -30,11 +30,15 @@ export const auth = (email, password) => {
       password: password,
       returnSecureToken: true,
     };
+    let url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB_JoQedROh5AmwQh0Z66I8MAmbE-b_TBs";
+    if (!isSignup) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB_JoQedROh5AmwQh0Z66I8MAmbE-b_TBs";
+    }
+
     axios
-      .post(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB_JoQedROh5AmwQh0Z66I8MAmbE-b_TBs",
-        authData
-      )
+      .post(url, authData)
       .then((response) => {
         console.log(response);
         dispatch(authSuccess(response.data));
