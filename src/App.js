174e838933a27c29eaad15d01a6918977a598b7d
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-07-01 14:24:31 -0400

    Add Autthentification form & routing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import { Route } from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
+import Auth from "./containers/Auth/Auth";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Layout>
           <Route path="/" exact component={BurgerBuilder} />
           <Route path="/checkout" component={Checkout} />
+          <Route path="/auth" component={Auth} />
           <Route path="/orders" component={Orders} />
         </Layout>
       </div>
