0baa13222cda3f482f7c98b4a52192e59b748df0
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-07-02 15:01:18 -0400

    Update the UI based on authentification status

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,13 +3,17 @@ import React from "react";
 import classes from "./NavigationItems.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
 
-const navigationItems = () => (
+const navigationItems = (props) => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link="/" exact>
       Burger Builder
     </NavigationItem>
     <NavigationItem link="/orders">Orders</NavigationItem>
-    <NavigationItem link="/auth">Log In</NavigationItem>
+    {!props.isAuthenticated ? (
+      <NavigationItem link="/auth">Log in</NavigationItem>
+    ) : (
+      <NavigationItem link="/logout">Log out</NavigationItem>
+    )}
   </ul>
 );
 
